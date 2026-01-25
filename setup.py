234d323042f8c9f6c62cac08a5ad24fea577a0f5
Harshithaaa01 <harshithasiddula2003@gmail.com>
2026-01-25 14:48:27 +0530

    Added Jenkins File

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='calculator',
+    version='1.0.0',
+    py_modules=['calculator'],
+    author='Siddula Harshitha',
+    description='Simple calculator module',
+)
